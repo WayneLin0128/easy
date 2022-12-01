91f73537b625cb2b77d3e746f91946e10ad50362
WayneLin0128 <waynelin0128@gmail.com>
2022-12-01 14:47:43 +0800

    fix Att need variable "shape"

--- NAM_resnet.py
+++ NAM_resnet.py
@@ -44,7 +44,7 @@ class BasicBlock(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
-        self.nam = Att(planes)
+        self.nam = Att(planes, in_planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1:
